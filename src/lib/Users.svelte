429544587bb65f6ed64d2b8fdb0b3631ee18826e
Joe DeSoto <modulusman@gmail.com>
2022-11-14 13:06:28 -0800

    Updates users component to loop through the given object and display UI elements from it.

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -3,35 +3,45 @@
   import { each } from "svelte/internal";
   let users = [
     {
+      id: 1,
       image: userAvatar,
       name: "John1",
       email: "johnsemail1@email.com",
     },
     {
+      id: 2,
       image: userAvatar,
       name: "John2",
       email: "johnsemail2@email.com",
     },
     {
+      id: 3,
       image: userAvatar,
       name: "John3",
       email: "johnsemail3@email.com",
     },
     {
+      id: 4,
       image: userAvatar,
       name: "John4",
       email: "johnsemail4@email.com",
     },
   ];
+
+  const getUsers = () => {
+    return users;
+  };
 </script>
 
 <div>
   <h1>List of Users</h1>
   <div>
-    {#each users as user}
-      <img src={user.image} alt={user.image} />
-      <p>{user.name}</p>
-      <p>{user.email}</p>
+    {#each getUsers() as { name, image, email, id }, index (id)}
+      <img src={image} alt={image} />
+      <p>{name}</p>
+      <p>{email}</p>
+    {:else}
+      <p>No users found!</p>
     {/each}
   </div>
 </div>
