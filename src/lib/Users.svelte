3fb22692e7beb4fe7673b65152ce5dbc727c80e7
Joe DeSoto <modulusman@gmail.com>
2022-11-14 11:04:59 -0800

    Adds Users component to the project and brings this in to App.svelte.

---
+++ src/lib/Users.svelte
@@ -0,0 +1,14 @@
+<script>
+  let userImage = "https://cdn-icons-png.flaticon.com/512/2202/2202112.png";
+  let userName = "John";
+  let userEmail = "johnsemail@email.com";
+</script>
+
+<div>
+  <h1>List of Users</h1>
+  <div>
+    <img src={userImage} alt={userName}>
+    <p>{userName}</p>
+    <p>{userEmail}</p>
+  </div>
+</div>
