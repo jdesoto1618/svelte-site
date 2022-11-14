8291faff51616820a5221ec7db4f662f5f92fd61
Joe DeSoto <modulusman@gmail.com>
2022-11-14 10:49:39 -0800

    Adds first user image, user name, and user email.
    Info is hardcoded to make sure it shows on the front end.

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<div>
+  <h1>List of Users</h1>
+  <div>
+    <img src="https://cdn-icons-png.flaticon.com/512/2202/2202112.png" alt="user avatar">
+    <p>User 1</p>
+    <p>user1@email.com</p>
+  </div>
+</div>
