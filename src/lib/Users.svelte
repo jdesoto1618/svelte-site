478c530377bcee8df35534582c243a6548235e2b
Joe DeSoto <modulusman@gmail.com>
2022-11-14 11:30:57 -0800

    Adds generic user avatar image to local project.
    Also adds each loop to iterate through the user object. This loop
    pulls all user info and displays it on the front end.

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,14 +1,37 @@
 <script>
-  let userImage = "https://cdn-icons-png.flaticon.com/512/2202/2202112.png";
-  let userName = "John";
-  let userEmail = "johnsemail@email.com";
+  import userAvatar from "../assets/user-image.png";
+  import { each } from "svelte/internal";
+  let users = [
+    {
+      image: userAvatar,
+      name: "John1",
+      email: "johnsemail1@email.com",
+    },
+    {
+      image: userAvatar,
+      name: "John2",
+      email: "johnsemail2@email.com",
+    },
+    {
+      image: userAvatar,
+      name: "John3",
+      email: "johnsemail3@email.com",
+    },
+    {
+      image: userAvatar,
+      name: "John4",
+      email: "johnsemail4@email.com",
+    },
+  ];
 </script>
 
 <div>
   <h1>List of Users</h1>
   <div>
-    <img src={userImage} alt={userName}>
-    <p>{userName}</p>
-    <p>{userEmail}</p>
+    {#each users as user}
+      <img src={user.image} alt={user.image} />
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+    {/each}
   </div>
 </div>
