135385238542d478eeda804406e1daa2d96e706b
Joe DeSoto <modulusman@gmail.com>
2022-11-17 14:07:48 -0800

    Adds routes to the site so people can view different pages.
    Sets up a homepage and a users page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script>
   import Users from "./lib/Users.svelte";
+  import { Router, Link, Route } from "svelte-navigator";
+  import Homepage from "./pages/Homepage.svelte";
+  import UsersPage from "./pages/UsersPage.svelte";
 </script>
 
-<Users />
+<Router>
+  <nav class="site-nav">
+    <Link to="/">Home</Link>
+    <Link to="/users">Users</Link>
+  </nav>
+
+  <Route path="/">
+    <Homepage />
+  </Route>
+
+  <Route path="/users">
+    <UsersPage />
+  </Route>
+</Router>
---
+++ src/pages/Homepage.svelte
@@ -0,0 +1,4 @@
+<div>
+  <h1>Welcome to the homepage!</h1>
+  <p>Cick the Users link to see the list of all users.</p>
+</div>
