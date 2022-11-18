0dac6da5f98c744d7a1125ab7fc3a13e14316c09
Joe DeSoto <modulusman@gmail.com>
2022-11-17 17:51:54 -0800

    Creates header on all pages, one for desktop and another for mobile.
    Mobile menu bars created and styled as well.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,27 @@
 <script>
   import Users from "./lib/Users.svelte";
+  import { onMount } from "svelte";
   import { Router, Link, Route } from "svelte-navigator";
   import Homepage from "./pages/Homepage.svelte";
   import UsersPage from "./pages/UsersPage.svelte";
+
+  onMount(() => {
+    const siteHeader = document.querySelector(".site-header");
+    const siteNav = document.querySelector(".site-nav.desktop");
+    siteHeader.append(siteNav);
+  });
 </script>
 
 <Router>
-  <nav class="site-nav">
-    <Link to="/">Home</Link>
-    <Link to="/users">Users</Link>
+  <nav class="site-nav desktop">
+    <ul class="site-nav-ul">
+      <li class="site-nav-li">
+        <Link class="site-nav-link" to="/">Home</Link>
+      </li>
+      <li class="site-nav-li">
+        <Link class="site-nav-link" to="/users">Users</Link>
+      </li>
+    </ul>
   </nav>
 
   <Route path="/">
